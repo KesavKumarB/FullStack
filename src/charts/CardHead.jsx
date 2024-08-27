cc2233061c188b1e43715cbac76a951bd153e26d
kesavkumar.b <kesavkumar.b@igosolutions.eu>
2024-08-27 12:36:54 +0530

    head

--- src/charts/CardHead.jsx
+++ src/charts/CardHead.jsx
@@ -4,7 +4,7 @@ const CardHead = () => {
   return (
     <>
       <div className="p-5 sm:text-2xl md:text-3xl lg:text-4xl font-bold text-red-600">
-        Card's Responsive Design
+        Card's Responsive Design using React and Tailwind CSS
       </div>
     </>
   );
--- src/charts/ChartHead.jsx
+++ src/charts/ChartHead.jsx
@@ -4,7 +4,7 @@ const ChartHead = () => {
   return (
     <>
       <div className="sm:p-2 md:p-5 sm:text-2xl md:text-3xl lg:text-4xl font-bold text-red-600">
-        Chart's Responsive Design
+        Chart's Responsive Design using React and Tailwind CSS
       </div>
     </>
   );
